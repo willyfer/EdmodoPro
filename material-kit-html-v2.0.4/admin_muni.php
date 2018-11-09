75630b2a145d2433a411dbf5c2c3170aa55a7fc0
willyfer <35695980+willyfer@users.noreply.github.com>
2018-11-09 08:38:58 -0500

    actualizacion
    v4

---
+++ material-kit-html-v2.0.4/admin_muni.php
@@ -0,0 +1,70 @@
+<!DOCTYPE doctype html>
+<meta charset="utf-8"/>
+<html lang="en">
+	<header>
+		<title>Administracion municipalidad de Ferreñafe</title>
+		 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	</header>
+	<body class="container-fluid">
+		<div>
+			<img src="#" alt="">
+			<h1 class="btn-primary" style="padding: 5px">Municipalidad de Ferreñafe</h1>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<button class="btn btn-primary btn">nuevo</button>
+				<button class="btn btn-primary btn">modificar</button>
+				<button class="btn btn-primary btn">eliminar</button>
+			</div>
+			<div class="col-md-8" >
+				
+				<table class="table table-dark">
+				  <thead class="thead-dark">
+				    <tr>
+				      <th scope="col">#</th>
+				      <th scope="col">Correo</th>
+				      <th scope="col">Nombre</th>
+				      <th scope="col">telefono</th>
+				      <th scope="col">seguimiento</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <th scope="row">1</th>
+				      <td>Mark</td>
+				      <td>Otto</td>
+				      <td>@mdo</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Jacob</td>
+				      <td>Thornton</td>
+				      <td>@fat</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">3</th>
+				      <td>Larry</td>
+				      <td>the Bird</td>
+				      <td>@twitter</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">3</th>
+				      <td>Larry</td>
+				      <td>the Bird</td>
+				      <td>@twitter</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">3</th>
+				      <td> dfdf</td>
+				      <td>the Bird</td>
+				      <td>76564432</td>
+				      <td><button class="btn-warning btn ">ver</button></td>
+				    </tr>
+				  </tbody>
+			</table>
+			</div>
+		 
+		</div>
+		
+	</body>
+</html>
