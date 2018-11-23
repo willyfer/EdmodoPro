d7dc0e5ce723e081348ba1352042aee9ec043b3a
willyfer <35695980+willyfer@users.noreply.github.com>
2018-11-22 22:32:00 -0500

    actualizacion
    v10

---
+++ material-kit-pro-html-v1.2.1/menu_alumno.php
@@ -0,0 +1,444 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" href="assets/img/favicon.png">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+	<title>Material Kit by Creative Tim</title>
+
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+	<!--     Fonts and icons     -->
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+
+	<!-- CSS Files -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/material-kit.css?v=1.2.1" rel="stylesheet"/>
+
+	<!-- CSS Just for demo purpose, don't include it in your project -->
+	<link href="assets/assets-for-demo/vertical-nav.css" rel="stylesheet" />
+	<link href="assets/assets-for-demo/demo.css" rel="stylesheet" />
+</head>
+	
+<body>
+	<header>
+		<nav class="navbar navbar-inverse">
+			   <div class="container">
+	                    <!-- Brand and toggle get grouped for better mobile display -->
+	                    <div class="navbar-header">
+	                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+	                        <span class="sr-only">Toggle navigation</span>
+	                        <span class="icon-bar"></span>
+	                        <span class="icon-bar"></span>
+	                        <span class="icon-bar"></span>
+	                      </button>
+	                     <img src="assets/img/alumno/logo2.png" class="img-responsive" style="width: 190px;height: 60px" alt="">
+	                    </div>
+
+	                <!-- Collect the nav links, forms, and other content for toggling -->
+	                    <div class="collapse navbar-collapse" id="navigation-example-2">
+	                      <ul class="nav navbar-nav navbar-right">
+	                            <li>
+	                                <a href="#pablo">
+	                                   Inicio
+	                                </a>
+	                            </li>
+	                            <li>
+	                                <a href="#pablo">
+	                                    Tareas
+	                                </a>
+	                            </li>
+	                            <li>
+	                                <a href="#pablo" class="btn btn-rose btn-raised btn-fab btn-fab-mini" data-toggle="dropdown">
+	                                    <i class="material-icons">email</i>
+	                                </a>
+	                            </li>
+	                            <li class="dropdown">
+	                                <a href="#pablo" class="profile-photo dropdown-toggle" data-toggle="dropdown">
+	                                    <div class="profile-photo-small">
+	                                        <img src="assets/img/faces/avatar.jpg" alt="Circle Image" class="img-circle img-responsive">
+	                                    </div>
+	                                </a>
+	                                <ul class="dropdown-menu">
+	                                    <li class="dropdown-header">
+	                                      Opciones
+	                                    </li>
+	                                    <li>
+	                                        <a href="#pablo">ver notas</a>
+	                                    </li>
+	                                    <li>
+	                                        <a href="#pablo">cambiar Imagen</a>
+	                                    </li>
+	                                    <li class="divider"></li>
+	                                    <li><a href="#pablo">Cerrar Sesion</a></li>
+	                                </ul>
+	                            </li>
+	                       </ul>
+	                    </div><!-- /.navbar-collapse -->
+	                  </div><!-- /.container-->
+	                
+	                  
+
+		</nav>
+	               
+	                
+	</header>
+	 
+		
+		<div class="row" style="margin: 16px">
+			<div class="col-md-3" style="background-color: black">
+				<div>
+					<h3>Cursos</h3>
+
+	    					 <ul class="nav nav-pills nav-pills-icons " role="tablist">
+	    						<li class="active">
+	    							<a href="#dashboard-1" role="tab" data-toggle="tab" aria-expanded="true">
+	    								<i class="material-icons">dashboard</i>
+	    								Matematica
+	    							</a>
+	    						</li>
+	    						<li class="">
+	                                <a href="#schedule-1" role="tab" data-toggle="tab" aria-expanded="false">
+	    								<i class="material-icons">schedule</i>
+	    								Comunicacion
+	                                </a>
+	                            </li>
+	                            <li class="">
+	                                <a href="#tasks-1" role="tab" data-toggle="tab" aria-expanded="false">
+	    								<i class="material-icons">list</i>
+	                                   Biologia
+	                                </a>
+	                            </li>
+	                          </ul>
+	                        
+				</div>
+				<div>	   
+					<h3>Tareas</h3>
+
+								<div class="header header-primary">
+									<!-- colors: "header-primary", "header-info", "header-success", "header-warning", "header-danger" -->
+									<div class="nav-tabs-navigation">
+										<div class="nav-tabs-wrapper">
+											<ul class="nav nav-tabs" data-tabs="tabs">
+												<li class="active">
+													<a href="#profile" data-toggle="tab" aria-expanded="true">
+														<i class="material-icons">face</i>
+														Por Curso
+													<div class="ripple-container"></div></a>
+												</li>
+												<li class="">
+													<a href="#messages" data-toggle="tab" aria-expanded="false">
+														<i class="material-icons">chat</i>
+														Por Fecha
+													<div class="ripple-container"></div></a>
+												</li>
+											 
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="card-content">
+									<div class="tab-content text-center">
+										<div class="tab-pane active" id="profile">
+											<p> I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. </p>
+										</div>
+										<div class="tab-pane" id="messages">
+											<p> I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at.</p>
+										</div>
+ 
+									</div>
+								</div>
+							
+				</div>
+			
+								 
+			 </div>
+			<div class="col-md-8">
+				
+		                <div class="title">
+		                    <h3>Publicaciones</h3>
+		                </div>
+		        		<div class="row" style="padding: 0px !important">
+		        			<div >
+		        				<div class="media-area">
+		        					 
+		        					<div class="media">
+		        						<a class="pull-left" href="#pablo">
+		        							<div class="avatar">
+		        								<img class="media-object" src="assets/img/faces/avatar.jpg" alt="...">
+		        							</div>
+		        						</a>
+		        						<div class="media-body">
+		        							<h4 class="media-heading">Tina Andrew <small>· 7 minutes ago</small></h4>
+		        							<h6 class="text-muted"></h6>
+
+		        							<p>Chance too good. God level bars. I'm so proud of @LifeOfDesiigner #1 song in the country. Panda! Don't be scared of the truth because we need to restart the human foundation in truth I stand with the most humility. We are so blessed!</p>
+		        							<p>All praises and blessings to the families of people who never gave up on dreams. Don't forget, You're Awesome!</p>
+
+		        							<div class="media-footer">
+		        								<a href="#pablo" class="btn btn-primary btn-simple pull-right" rel="tooltip" title="" data-original-title="Reply to Comment">
+		        									<i class="material-icons">reply</i> Reply
+		        								<div class="ripple-container"></div></a>
+		        								<a href="#pablo" class="btn btn-danger btn-simple pull-right">
+		        									<i class="material-icons">favorite</i> 243
+		        								<div class="ripple-container"></div></a>
+		        							</div>
+
+		        							<div class="media media-post">
+		        								<a class="pull-left author" href="#pablo">
+		        									<div class="avatar">
+		        										<img class="media-object" alt="64x64" src="assets/img/faces/kendall.jpg">
+		        									</div>
+		        								</a>
+		        								<div class="media-body">
+		        										<div class="form-group is-empty"><textarea class="form-control" placeholder="Write a nice reply or go home..." rows="4"></textarea><span class="material-input"></span></div>
+		        										<div class="media-footer">
+		        											<a href="#pablo" class="btn btn-primary pull-right">
+		        												<i class="material-icons">reply</i> Reply
+		        											<div class="ripple-container"></div></a>
+		        										</div>
+		        								</div>
+		        							</div>
+		        						</div>
+		        					</div>
+
+		        					<div class="media">
+		        					  <a class="pull-left" href="#pablo">
+		        						  <div class="avatar">
+		        							 <img class="media-object" alt="Tim Picture" src="assets/img/faces/marc.jpg">
+		        						  </div>
+		        					  </a>
+		        					  <div class="media-body">
+		        						 <h4 class="media-heading">John Camber <small>· Yesterday</small></h4>
+
+		        						 <p>Hello guys, nice to have you on the platform! There will be a lot of great stuff coming soon. We will keep you posted for the latest news.</p>
+		        						 <p> Don't forget, You're Awesome!</p>
+
+		        						<div class="media-footer">
+		        							<a href="#pablo" class="btn btn-primary btn-simple pull-right" rel="tooltip" title="" data-original-title="Reply to Comment">
+		        								<i class="material-icons">reply</i> Reply
+		        							</a>
+		        							<a href="#pablo" class="btn btn-default btn-simple pull-right">
+		        								<i class="material-icons">favorite</i> 25
+		        							</a>
+		        						 </div>
+		        						 <div class="media">
+		        							  <a class="pull-left" href="#pablo">
+		        									<div class="avatar">
+		        										<img class="media-object" alt="64x64" src="assets/img/faces/avatar.jpg">
+		        									</div>
+		        							  </a>
+		        							  <div class="media-body">
+		        									<h4 class="media-heading">Tina Andrew <small>· 2 Days Ago</small></h4>
+
+		        									<p>Hello guys, nice to have you on the platform! There will be a lot of great stuff coming soon. We will keep you posted for the latest news.</p>
+		        									<p> Don't forget, You're Awesome!</p>
+
+		        									<div class="media-footer">
+		        										<a href="#pablo" class="btn btn-primary btn-simple pull-right" rel="tooltip" title="" data-original-title="Reply to Comment">
+		        											<i class="material-icons">reply</i> Reply
+		        										</a>
+		        										<a href="#pablo" class="btn btn-danger btn-simple pull-right">
+		        											<i class="material-icons">favorite</i> 243
+		        										</a>
+		        									</div>
+		        							  </div>
+		        						  </div>
+		        					  </div>
+		        					</div>
+
+		        					<div class="media">
+		        						  <a class="pull-left" href="#pablo">
+		        							  <div class="avatar">
+		        									<img class="media-object" alt="64x64" src="assets/img/faces/kendall.jpg">
+		        							  </div>
+		        						  </a>
+		        						  <div class="media-body">
+		        								<h4 class="media-heading">Rosa Thompson <small>· 2 Days Ago</small></h4>
+
+		        								<p>Hello guys, nice to have you on the platform! There will be a lot of great stuff coming soon. We will keep you posted for the latest news.</p>
+		        								<p> Don't forget, You're Awesome!</p>
+
+		        								<div class="media-footer">
+		        									<a href="#pablo" class="btn btn-primary btn-simple pull-right" rel="tooltip" title="" data-original-title="Reply to Comment">
+		        										<i class="material-icons">reply</i> Reply
+		        									</a>
+		        									<a href="#pablo" class="btn btn-danger btn-simple pull-right">
+		        										<i class="material-icons">favorite</i> 243
+		        									</a>
+		        								</div>
+		        						  </div>
+		        					</div>
+
+		        				 
+		        				</div>
+
+ 
+		        			</div>
+		        		</div>
+		            
+
+			</div>
+	 
+		
+ </div>
+	 
+	<footer class="footer footer-black footer-big">
+			<div class="container">
+
+							<div class="content">
+								<div class="row">
+									<div class="col-md-4">
+										<h5>About Us</h5>
+										<p>Creative Tim is a startup that creates design tools that make the web development process faster and easier. </p> <p>We love the web and care deeply for how users interact with a digital product. We power businesses and individuals to create better looking web projects around the world. </p>
+									</div>
+
+									<div class="col-md-4">
+										<h5>Social Feed</h5>
+										<div class="social-feed">
+											<div class="feed-line">
+												<i class="fa fa-twitter"></i>
+												<p>How to handle ethical disagreements with your clients.</p>
+											</div>
+											<div class="feed-line">
+												<i class="fa fa-twitter"></i>
+												<p>The tangible benefits of designing at 1x pixel density.</p>
+											</div>
+											<div class="feed-line">
+												<i class="fa fa-facebook-square"></i>
+												<p>A collection of 25 stunning sites that you can use for inspiration.</p>
+											</div>
+										</div>
+									</div>
+
+									<div class="col-md-4">
+										<h5>Instagram Feed</h5>
+										<div class="gallery-feed">
+											<img src="assets/img/faces/card-profile6-square.jpg" class="img img-raised img-rounded" alt="">
+											<img src="assets/img/faces/christian.jpg" class="img img-raised img-rounded" alt="">
+											<img src="assets/img/faces/card-profile4-square.jpg" class="img img-raised img-rounded" alt="">
+											<img src="assets/img/faces/card-profile1-square.jpg" class="img img-raised img-rounded" alt="">
+
+											<img src="assets/img/faces/marc.jpg" class="img img-raised img-rounded" alt="">
+											<img src="assets/img/faces/kendall.jpg" class="img img-raised img-rounded" alt="">
+											<img src="assets/img/faces/card-profile5-square.jpg" class="img img-raised img-rounded" alt="">
+											<img src="assets/img/faces/card-profile2-square.jpg" class="img img-raised img-rounded" alt="">
+										</div>
+
+									</div>
+								</div>
+							</div>
+
+
+							<hr>
+
+							<ul class="pull-left">
+								<li>
+									<a href="#pablo">
+									   Blog
+									</a>
+								</li>
+								<li>
+									<a href="#pablo">
+										Presentation
+									</a>
+								</li>
+								<li>
+									<a href="#pablo">
+									   Discover
+									</a>
+								</li>
+								<li>
+									<a href="#pablo">
+										Payment
+									</a>
+								</li>
+								<li>
+									<a href="#pablo">
+										Contact Us
+									</a>
+								</li>
+							</ul>
+
+							<div class="copyright pull-right">
+								Copyright © <script>document.write(new Date().getFullYear())</script>2018 Creative Tim All Rights Reserved.
+							</div>
+						</div>
+	</footer>
+	 
+					
+		 
+			  
+	
+</body>
+
+<script src="assets/js/jquery.min.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="assets/js/material.min.js"></script>
+
+	<!--  Plugin for Date Time Picker and Full Calendar Plugin-->
+	<script src="assets/js/moment.min.js"></script>
+
+	<!--	Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+	<script src="assets/js/nouislider.min.js" type="text/javascript"></script>
+
+	<!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+	<script src="assets/js/bootstrap-datetimepicker.js" type="text/javascript"></script>
+
+	<!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+	<script src="assets/js/bootstrap-selectpicker.js" type="text/javascript"></script>
+
+	<!--	Plugin for Tags, full documentation here: http://xoxco.com/projects/code/tagsinput/  -->
+	<script src="assets/js/bootstrap-tagsinput.js"></script>
+
+	<!--	Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+	<script src="assets/js/jasny-bootstrap.min.js"></script>
+
+	<!-- Plugin For Google Maps -->
+	<script  type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+
+
+
+	<!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc -->
+	<script src="assets/js/material-kit.js?v=1.2.1" type="text/javascript"></script>
+
+	<!-- Fixed Sidebar Nav - JS For Demo Purpose, Don't Include it in your project -->
+	<script src="assets/assets-for-demo/modernizr.js" type="text/javascript"></script>
+	<script src="assets/assets-for-demo/vertical-nav.js" type="text/javascript"></script>
+	<script type="text/javascript">
+
+		$(document).ready(function(){
+			var slider = document.getElementById('sliderRegular');
+
+	        noUiSlider.create(slider, {
+	            start: 40,
+	            connect: [true,false],
+	            range: {
+	                min: 0,
+	                max: 100
+	            }
+	        });
+
+	        var slider2 = document.getElementById('sliderDouble');
+
+	        noUiSlider.create(slider2, {
+	            start: [ 20, 60 ],
+	            connect: true,
+	            range: {
+	                min:  0,
+	                max:  100
+	            }
+	        });
+
+
+
+			materialKit.initFormExtendedDatetimepickers();
+
+		});
+	</script>
+
+ 
+</html>
