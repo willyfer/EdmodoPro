989b83bf89f09ac62b4bee38988dfc63942b156a
willyfer <35695980+willyfer@users.noreply.github.com>
2018-11-23 19:40:19 -0500

    actualizacion
    v11

--- material-kit-pro-html-v1.2.1/menu_alumno.php
+++ material-kit-pro-html-v1.2.1/menu_alumno.php
@@ -6,7 +6,7 @@
 	<link rel="icon" type="image/png" href="assets/img/favicon.png">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<title>Material Kit by Creative Tim</title>
+	<title>Sistema Academico alumno</title>
 
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
@@ -21,6 +21,7 @@
 	<!-- CSS Just for demo purpose, don't include it in your project -->
 	<link href="assets/assets-for-demo/vertical-nav.css" rel="stylesheet" />
 	<link href="assets/assets-for-demo/demo.css" rel="stylesheet" />
+	<link rel="stylesheet" href="assets/css/indice.css">
 </head>
 	
 <body>
@@ -89,74 +90,77 @@
 	 
 		
 		<div class="row" style="margin: 16px">
-			<div class="col-md-3" style="background-color: black">
-				<div>
-					<h3>Cursos</h3>
-
-	    					 <ul class="nav nav-pills nav-pills-icons " role="tablist">
-	    						<li class="active">
-	    							<a href="#dashboard-1" role="tab" data-toggle="tab" aria-expanded="true">
-	    								<i class="material-icons">dashboard</i>
-	    								Matematica
-	    							</a>
-	    						</li>
-	    						<li class="">
-	                                <a href="#schedule-1" role="tab" data-toggle="tab" aria-expanded="false">
-	    								<i class="material-icons">schedule</i>
-	    								Comunicacion
-	                                </a>
-	                            </li>
-	                            <li class="">
-	                                <a href="#tasks-1" role="tab" data-toggle="tab" aria-expanded="false">
-	    								<i class="material-icons">list</i>
-	                                   Biologia
-	                                </a>
-	                            </li>
-	                          </ul>
-	                        
-				</div>
-				<div>	   
-					<h3>Tareas</h3>
-
-								<div class="header header-primary">
-									<!-- colors: "header-primary", "header-info", "header-success", "header-warning", "header-danger" -->
-									<div class="nav-tabs-navigation">
-										<div class="nav-tabs-wrapper">
-											<ul class="nav nav-tabs" data-tabs="tabs">
-												<li class="active">
-													<a href="#profile" data-toggle="tab" aria-expanded="true">
-														<i class="material-icons">face</i>
-														Por Curso
-													<div class="ripple-container"></div></a>
-												</li>
-												<li class="">
-													<a href="#messages" data-toggle="tab" aria-expanded="false">
-														<i class="material-icons">chat</i>
-														Por Fecha
-													<div class="ripple-container"></div></a>
-												</li>
-											 
-											</ul>
-										</div>
-									</div>
-								</div>
-								<div class="card-content">
-									<div class="tab-content text-center">
-										<div class="tab-pane active" id="profile">
-											<p> I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. </p>
-										</div>
-										<div class="tab-pane" id="messages">
-											<p> I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at.</p>
-										</div>
+			<div class="col-md-3" style="background-color: ">
+				 <div>	  <table class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th class="text-center">
+                                                       
+                                                    </th>
+                                                    <th>
+                                                        <h3>Cusos</h3>
+                                                    </th>
+                                                    
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td class="text-center">
+                                                        1
+                                                    </td>
+                                                    <td>
+                                                      <a href=""> Matematica</a>
+                                                    </td>
+                                                    
+                                                     
+                                                   
+                                                </tr>
+                                                <tr>
+                                                    <td class="text-center">
+                                                        2
+                                                    </td>
+                                                    <td>
+                                                        <a href="">Comunicacion</a>
+                                                    </td>
+                                                  
+                                                 
+                                                </tr>
+                                                <tr>
+                                                    <td class="text-center">
+                                                        3
+                                                    </td>
+                                                    <td>
+                                                      <a href="">Fisica</a> 
+                                                    </td>
+                                                   
+                                                </tr>
+                                                <tr>
+                                                    <td class="text-center">
+                                                        4
+                                                    </td>
+                                                    <td>
+                                                       <a href=""> Historia</a>
+                                                    </td>
+                                                    
+                                                  
+                                                </tr>
+                                                <tr>
+                                                    <td class="text-center">
+                                                        5
+                                                    </td>
+                                                    <td>
+                                                      <a href="">  Biologia</a>
+                                                    </td>
+                                                     
+                                              
+                                                </tr>
+                                            </tbody>
+                      </table>
+				 </div>
  
-									</div>
-								</div>
-							
-				</div>
-			
 								 
 			 </div>
-			<div class="col-md-8">
+			<div class="col-md-6">
 				
 		                <div class="title">
 		                    <h3>Publicaciones</h3>
@@ -180,26 +184,29 @@
 
 		        							<div class="media-footer">
 		        								<a href="#pablo" class="btn btn-primary btn-simple pull-right" rel="tooltip" title="" data-original-title="Reply to Comment">
-		        									<i class="material-icons">reply</i> Reply
+		        									<i class="material-icons">reply</i> comentar
 		        								<div class="ripple-container"></div></a>
 		        								<a href="#pablo" class="btn btn-danger btn-simple pull-right">
 		        									<i class="material-icons">favorite</i> 243
 		        								<div class="ripple-container"></div></a>
 		        							</div>
 
-		        							<div class="media media-post">
-		        								<a class="pull-left author" href="#pablo">
-		        									<div class="avatar">
-		        										<img class="media-object" alt="64x64" src="assets/img/faces/kendall.jpg">
+		        							<div class="media media-post ">
+		        								<a class="pull-left author  " href="#pablo">
+		        									<div class="avatar img">
+		        										<img class="media-object  " alt="64x64" src="assets/img/faces/kendall.jpg">
 		        									</div>
 		        								</a>
 		        								<div class="media-body">
-		        										<div class="form-group is-empty"><textarea class="form-control" placeholder="Write a nice reply or go home..." rows="4"></textarea><span class="material-input"></span></div>
-		        										<div class="media-footer">
-		        											<a href="#pablo" class="btn btn-primary pull-right">
+		        										<div class="form-group is-empty row " style="display: flex;">
+		        											<textarea class="form-control "  placeholder="Comentar algo..." rows="1"></textarea>
+		        											<a href="#pablo" class="btn  btn-primary pull-right">
 		        												<i class="material-icons">reply</i> Reply
-		        											<div class="ripple-container"></div></a>
+		        													 
+		        											</a>
+		        											<span class="material-input"></span>
 		        										</div>
+		        										 
 		        								</div>
 		        							</div>
 		        						</div>
@@ -219,10 +226,10 @@
 
 		        						<div class="media-footer">
 		        							<a href="#pablo" class="btn btn-primary btn-simple pull-right" rel="tooltip" title="" data-original-title="Reply to Comment">
-		        								<i class="material-icons">reply</i> Reply
+		        								<i class="material-icons">reply</i> Comentar
 		        							</a>
 		        							<a href="#pablo" class="btn btn-default btn-simple pull-right">
-		        								<i class="material-icons">favorite</i> 25
+		        								<i class="material-icons">Like</i> 25
 		        							</a>
 		        						 </div>
 		        						 <div class="media">
@@ -264,7 +271,7 @@
 
 		        								<div class="media-footer">
 		        									<a href="#pablo" class="btn btn-primary btn-simple pull-right" rel="tooltip" title="" data-original-title="Reply to Comment">
-		        										<i class="material-icons">reply</i> Reply
+		        										<i class="material-icons">comentar</i> Reply
 		        									</a>
 		        									<a href="#pablo" class="btn btn-danger btn-simple pull-right">
 		        										<i class="material-icons">favorite</i> 243
@@ -282,9 +289,103 @@
 		            
 
 			</div>
+			<div class="col-md-3">
+				<h3>Tareas</h3>
+				 	 <table class="table table-responsive">
+				 	 	 
+				 	 	<thead>
+				 	 		<tr>
+				 	 			 <th>Matematica</th>
+				 	 			 
+				 	 		</tr>
+				 	 	</thead>
+				 	 	<tbody>
+				 	 		<tr>
+				 	 			<td>titulo del trabajo
+				 	 			</td>
+				 	 			<td>
+
+                                <button class="btn  btn-xs btn-round btn-info " data-target="#noticeModal" data-toggle="modal">
+                                    <i class="material-icons">
+                                        info_outline
+                                    </i>
+                                    Detalles
+                                <div class="ripple-container"></div></button>
+                            
+                                </td>
+				 	 		</tr>
+				 	 	</tbody>
+
+				 	 </table>
+				 
+               
+			</div>
 	 
 		
  </div>
+ <div class="modal fade" id="noticeModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-notice">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="material-icons">clear</i></button>
+        <h5 class="modal-title" id="myModalLabel">Matematica </h5><br>
+        <div class="row">	
+			
+	       <div class="col-xs-2">
+	       	  <img src="assets/img/faces/kendall.jpg" alt="assets/img/faces/kendall.jpg" class="img-circle img-rounded " style="width: 60px;height: 60px">
+	       </div>
+	     
+	       <h4  >Rosa saavedra<small  >. Publicado el  12/4/2018</small> </h4>  
+        
+        </div>
+
+      </div>
+      <div class="modal-body">
+      	<h4>Titulo de la Tarea</h4>
+        <div class="instruction">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam eaque eum omnis atque illo alias commodi sint ipsa odit corporis vero quasi dignissimos veritatis accusantium ratione, adipisci dicta tenetur esse!</p>
+        </div>
+        <label for=""  style="color: #61589e">Fecha límite de entrega</label>
+        <label for="" style="color: #61589e">12/12/2018</label><br>
+        <label for="" style="color: #61589e">estado: </label>
+        <label for="" style="color: #61589e">No entregado</label>
+       
+        <div class="row">
+        	
+        </div>
+        <div  class="row">
+        	<textarea name="" id="" cols="30" rows="2" class="col-md-6"></textarea>
+         <div class="form-group form-file-upload is-empty is-fileinput  col-md-6">
+
+            <input id="inputFile2" multiple="" type="file">
+                <div class="input-group">
+
+                    <input class="form-control" placeholder="Arrastra aquí  tu archivo" style="background-color: gray" readonly="" type="text">
+                        <span class="input-group-btn input-group-s">
+                            <button class="btn btn-just-icon btn-round btn-primary" type="button">
+                               <i class="material-icons">
+                                    attach_file
+                                </i>
+                            </button>
+                        </span>
+                                            
+                </div>
+                                    
+           <span class="material-input"></span>
+
+        </div>
+	
+        </div>
+        
+      </div>
+      <div class="modal-footer text-center">
+            
+            <button class="btn btn-success">enviar</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
 	 
 	<footer class="footer footer-black footer-big">
 			<div class="container">
@@ -398,7 +499,7 @@
 	<script src="assets/js/jasny-bootstrap.min.js"></script>
 
 	<!-- Plugin For Google Maps -->
-	<script  type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+	 
 
 
 
